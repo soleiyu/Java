b1e36f06944a4b96356ada5f2a6c553ea6aafa4b
soleiyu <39758438+soleiyu@users.noreply.github.com>
2019-03-05 00:37:50 +0900

    Update otherUtil.java

--- pictFuncs/otherUtil.java
+++ pictFuncs/otherUtil.java
@@ -2,39 +2,36 @@ import java.util.Random;
 
 public final class otherUtil
 {
-    public static int getRand(int limit)
-    {
-	Random rd = new Random();
+	public static int getRand(int limit)
+	{
+		Random rd = new Random();
+		return rd.nextInt(limit);
+	}
 
-	return rd.nextInt(limit);
-    }
-
-    public static int getRandomPosx(pict canv, pict item, int ofs)
-    {
-	int left = item.width - ofs;
-	int right = canv.width - ofs;
-
-	int range = left + right;
+	public static int getRandomPosx(pict canv, pict item, int ofs)
+	{
+		int left = item.width - ofs;
+		int right = canv.width - ofs;
+		int range = left + right;
 	
-	Random rd = new Random();
+		Random rd = new Random();
 
-	int r = rd.nextInt(range);
+		int r = rd.nextInt(range);
 	
-	return r - left;
-    } 
+		return r - left;
+	}
 
-    public static int getRandomPosy(pict canv, pict item, int ofs)
-    {
-	int up = item.height - ofs;
-	int bt = canv.height - ofs;
+	public static int getRandomPosy(pict canv, pict item, int ofs)
+	{
+		int up = item.height - ofs;
+		int bt = canv.height - ofs;
 
-	int range = up + bt;
+		int range = up + bt;
 	
-	Random rd = new Random();
+		Random rd = new Random();
 
-	int r = rd.nextInt(range);
+		int r = rd.nextInt(range);
 	
-	return r - up;
-    }
-
+		return r - up;
+	}
 }
